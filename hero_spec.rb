fc9c165f360534d0021462a48ac6037ff6b38740
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-09 18:31:09 +0300

    Refactoring tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,26 +1,24 @@
 require './hero'
 
 describe Hero do
+  before do
+   @hero = Hero.new 'miles'
+  end
 
   it "has a capitalized name" do
-    hero = Hero.new 'miles'
-
-    expect(hero.name).to eq 'Miles'
+    expect(@hero.name).to eq 'Miles'
   end
 
   it "can power up" do
-    hero = Hero.new 'miles'
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down" do
-    hero = Hero.new 'miles'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
   
   it "displays full hero info" do
-    hero = Hero.new 'miles'
-    expect(hero.hero_info).to eq 'Miles has a health of 100'
+    expect(@hero.hero_info).to eq 'Miles has a health of 100'
   end
 
 end
